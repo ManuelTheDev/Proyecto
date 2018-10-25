e2f5af1ed1b351dc120e00300e16c549b9197d60
alulab14 <alulab14@inf.pucp.edu.pe>
2018-10-24 21:19:40 -0500

    Modifiqué TratamientoDA

--- ProyectoAshpana/Ashpana/AccesoDatos/TratamientoDA.cs
+++ ProyectoAshpana/Ashpana/AccesoDatos/TratamientoDA.cs
@@ -11,8 +11,12 @@ namespace AccesoDatos
 {
     public class TratamientoDA
     {
+       
         public BindingList<Tratamiento> listarTratamientos()
         {
+            CondicionMedicaDA condMedDA = new CondicionMedicaDA();
+            ZonaDA zonaDA = new ZonaDA();
+
             BindingList<Tratamiento> tratamientos = new BindingList<Tratamiento>();
             String cadena = "server=quilla.lab.inf.pucp.edu.pe;" + "user=inf282g4;" + "password=GvZf6p;" + "database=inf282g4;" + "port=3306;" + "SslMode=none;";
             MySqlConnection con = new MySqlConnection(cadena);
@@ -42,60 +46,80 @@ namespace AccesoDatos
                 else
                     trat.TipoString = "Corporal";
 
-                MySqlConnection con2 = new MySqlConnection(cadena);
-                con2.Open();
-                MySqlCommand comando2 = new MySqlCommand();
-                comando2.CommandType = System.Data.CommandType.StoredProcedure;
-                comando2.CommandText = "LISTAR_ZONAS_X_TRATAMIENTO";
-                comando2.Connection = con2;
-                comando2.Parameters.Add("_ID_TRATAMIENTO", MySqlDbType.Int32).Value = trat.IdTrat;
+                trat.CondicionesMedicas = condMedDA.listarCondicionesMedicas_X_Tratamiento(trat.IdTrat);
 
-                MySqlDataReader lector2 = comando2.ExecuteReader();
-
-                while (lector2.Read())
-                {
-                    Zona zona = new Zona();
-                    zona.IdZona = lector2.GetInt32("ID_ZONA");
-                    zona.NombreZona = lector2.GetString("NOMBRE");
-                    zona.DescripicionZona = lector2.GetString("DESCRIPCION");
-                    trat.ZonasTratar.Add(zona);
-                }
-                con2.Close();
                 String cad = "";
-                foreach (Zona z in trat.ZonasTratar)
-                {
-                    cad = cad + z.NombreZona + "/ ";
-
-                }
-
-                trat.ZonasTratarString = cad;
-
-                MySqlConnection con3 = new MySqlConnection(cadena);
-                con3.Open();
-                MySqlCommand comando3 = new MySqlCommand();
-                comando3.CommandType = System.Data.CommandType.StoredProcedure;
-                comando3.CommandText = "LISTAR_CONDICIONES_MEDICAS_X_TRATAMIENTO";
-                comando3.Connection = con3;
-                comando3.Parameters.Add("_ID_TRATAMIENTO", MySqlDbType.Int32).Value = trat.IdTrat;
-
-                MySqlDataReader lector3 = comando3.ExecuteReader();
-
-                while (lector3.Read())
-                {
-                    CondicionMedica condMed = new CondicionMedica();
-                    condMed.IdCondMed = lector3.GetInt32("ID_CONDICION_MEDICA");
-                    condMed.Nombre = lector3.GetString("NOMBRE");
-                    condMed.Descripcion = lector3.GetString("DESCRIPCION");
-                    trat.CondicionesMedicas.Add(condMed);
-                }
-                con3.Close();
-                String cad2 = "";
                 foreach (CondicionMedica c in trat.CondicionesMedicas)
                 {
-                    cad2 = cad2 + c.Nombre + "/ ";
+                    cad = cad + c.Nombre + "/ ";
 
                 }
-                trat.CondicionesMedicasString = cad2;
+
+                trat.CondicionesMedicasString = cad;
+
+                trat.ZonasTratar = zonaDA.listarZonas_X_Tratamiento(trat.IdTrat);
+                String cad2 = "";
+                foreach (Zona z in trat.ZonasTratar)
+                {
+                    cad2 = cad2 + z.NombreZona + "/ ";
+
+                }
+                trat.ZonasTratarString = cad2;
+
+                /* MySqlConnection con2 = new MySqlConnection(cadena);
+                 con2.Open();
+                 MySqlCommand comando2 = new MySqlCommand();
+                 comando2.CommandType = System.Data.CommandType.StoredProcedure;
+                 comando2.CommandText = "LISTAR_ZONAS_X_TRATAMIENTO";
+                 comando2.Connection = con2;
+                 comando2.Parameters.Add("_ID_TRATAMIENTO", MySqlDbType.Int32).Value = trat.IdTrat;
+
+                 MySqlDataReader lector2 = comando2.ExecuteReader();
+
+                 while (lector2.Read())
+                 {
+                     Zona zona = new Zona();
+                     zona.IdZona = lector2.GetInt32("ID_ZONA");
+                     zona.NombreZona = lector2.GetString("NOMBRE");
+                     zona.DescripicionZona = lector2.GetString("DESCRIPCION");
+                     trat.ZonasTratar.Add(zona);
+                 }
+                 con2.Close();
+                 String cad = "";
+                 foreach (Zona z in trat.ZonasTratar)
+                 {
+                     cad = cad + z.NombreZona + "/ ";
+
+                 }
+
+                 trat.ZonasTratarString = cad;
+
+                 MySqlConnection con3 = new MySqlConnection(cadena);
+                 con3.Open();
+                 MySqlCommand comando3 = new MySqlCommand();
+                 comando3.CommandType = System.Data.CommandType.StoredProcedure;
+                 comando3.CommandText = "LISTAR_CONDICIONES_MEDICAS_X_TRATAMIENTO";
+                 comando3.Connection = con3;
+                 comando3.Parameters.Add("_ID_TRATAMIENTO", MySqlDbType.Int32).Value = trat.IdTrat;
+
+                 MySqlDataReader lector3 = comando3.ExecuteReader();
+
+                 while (lector3.Read())
+                 {
+                     CondicionMedica condMed = new CondicionMedica();
+                     condMed.IdCondMed = lector3.GetInt32("ID_CONDICION_MEDICA");
+                     condMed.Nombre = lector3.GetString("NOMBRE");
+                     condMed.Descripcion = lector3.GetString("DESCRIPCION");
+                     trat.CondicionesMedicas.Add(condMed);
+                 }
+                 con3.Close();
+                 String cad2 = "";
+                 foreach (CondicionMedica c in trat.CondicionesMedicas)
+                 {
+                     cad2 = cad2 + c.Nombre + "/ ";
+
+                 }
+                 trat.CondicionesMedicasString = cad2;*/
                 tratamientos.Add(trat);
             }
 
