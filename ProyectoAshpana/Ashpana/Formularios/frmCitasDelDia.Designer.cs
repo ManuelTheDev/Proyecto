cf3f78a33208985262f81dd529bda293b54c8f9a
SrPicadillo <j.nicolas.due@gmail.com>
2018-11-26 20:04:08 -0500

    Añadido buscar a citas del día

--- ProyectoAshpana/Ashpana/Formularios/frmCitasDelDia.Designer.cs
+++ ProyectoAshpana/Ashpana/Formularios/frmCitasDelDia.Designer.cs
@@ -33,14 +33,14 @@
             this.CITAS = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvCitas = new System.Windows.Forms.DataGridView();
-            this.txtBuscar = new System.Windows.Forms.TextBox();
-            this.lblBuscar = new System.Windows.Forms.Label();
             this.colPaciente = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colServicio = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colHora = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTerapista = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colSesion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblBuscar = new System.Windows.Forms.Label();
             this.CITAS.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).BeginInit();
             this.SuspendLayout();
@@ -60,11 +60,11 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(670, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 44);
+            this.label1.Size = new System.Drawing.Size(281, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "CITAS DEL DÍA";
             // 
@@ -78,7 +78,7 @@
             this.dgvCitas.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.YellowGreen;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
@@ -94,7 +94,7 @@
             this.colSesion});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
@@ -107,23 +107,6 @@
             this.dgvCitas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvCitas.Size = new System.Drawing.Size(768, 181);
             this.dgvCitas.TabIndex = 17;
-            // 
-            // txtBuscar
-            // 
-            this.txtBuscar.Location = new System.Drawing.Point(266, 132);
-            this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(670, 20);
-            this.txtBuscar.TabIndex = 19;
-            // 
-            // lblBuscar
-            // 
-            this.lblBuscar.AutoSize = true;
-            this.lblBuscar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBuscar.Location = new System.Drawing.Point(164, 132);
-            this.lblBuscar.Name = "lblBuscar";
-            this.lblBuscar.Size = new System.Drawing.Size(78, 21);
-            this.lblBuscar.TabIndex = 18;
-            this.lblBuscar.Text = "BUSCAR:";
             // 
             // colPaciente
             // 
@@ -171,6 +154,24 @@
             this.colSesion.ReadOnly = true;
             this.colSesion.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
             this.colSesion.Width = 80;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(266, 132);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(670, 20);
+            this.txtBuscar.TabIndex = 19;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.Location = new System.Drawing.Point(164, 132);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(81, 20);
+            this.lblBuscar.TabIndex = 18;
+            this.lblBuscar.Text = "BUSCAR:";
             // 
             // frmCitasDelDia
             // 
--- ProyectoAshpana/Ashpana/Formularios/frmCitasDelDia.cs
+++ ProyectoAshpana/Ashpana/Formularios/frmCitasDelDia.cs
@@ -1,4 +1,5 @@
 ﻿using LogicaNegocio;
+using Modelo;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,12 +16,43 @@ namespace Formularios
     public partial class frmCitasDelDia : Form
     {
         private SesionBL sesionBL;
+        private BindingList<Sesion> sesiones;
+
         public frmCitasDelDia()
         {
             InitializeComponent();
             sesionBL = new SesionBL();
             dgvCitas.AutoGenerateColumns = false;
-            dgvCitas.DataSource = sesionBL.listarSesionesDelDia();
+            CargarDGV();
+        }
+
+        private void CargarDGV()
+        {
+            sesiones = sesionBL.listarSesionesDelDia();
+            dgvCitas.DataSource = sesiones;
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            string busqueda = txtBuscar.Text;
+            var sesionesBuscadas = new BindingList<Sesion>();
+
+            string patron = "";
+            for (int i = 0; i < busqueda.Length; i++)
+                if (('A' <= busqueda[i] && busqueda[i] <= 'Z') || ('a' <= busqueda[i] && busqueda[i] <= 'z')) patron += "[" + System.Char.ToLower(busqueda[i]) + System.Char.ToUpper(busqueda[i]) + "]";
+                else patron += busqueda[i];
+            
+            Regex rgx = new Regex(@"" + patron + @"");
+            foreach (Sesion s in sesiones)
+            {
+                if (rgx.IsMatch(s.NomCliente))
+                {
+                    sesionesBuscadas.Add(s);
+                }
+
+            }
+
+            dgvCitas.DataSource = sesionesBuscadas;
         }
     }
 }
